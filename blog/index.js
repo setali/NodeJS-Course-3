f812efcdd5c1f7c82dceb9e9c4f3b617ce40fb7e
setali <set.ali.mousavi@gmail.com>
2023-09-08 09:26:11 +0330

    2-routing

--- blog/index.js
+++ blog/index.js
@@ -1,45 +1,9 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log(req.url)
-
-  next()
-})
-
-app.use((req, res, next) => {
-  //   throw new Error('Middleware Error')
-  next()
-})
-
-app.use('/about*', (req, res, next) => {
-  console.log('About middleware')
-
-  //   return res.send('Alo')
-  next()
-})
-
-app.get('/', (req, res) => {
-  //   throw new Error('Homepage error')
-
-  res.send('Hello word')
-})
-
-app.get('/about', (req, res) => {
-  //   res.statusCode = 202
-  res.status(202).send('About us')
-})
-
-app.get('/api', (req, res) => {
-  console.log(req.get('host'))
-  return res.json({ name: 'ali' })
-  console.log('salam')
-})
-
-app.get('/user/*', (req, res) => {
-  return res.send('User')
-})
+app.use(routes)
 
 app.all('*', (req, res) => {
   res.status(404).send('Not Found')
