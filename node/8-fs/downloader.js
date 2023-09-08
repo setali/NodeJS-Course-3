f4b92c274abe40824ff8d6c12357a9e854df228f
setali <set.ali.mousavi@gmail.com>
2023-09-08 13:40:33 +0330

    Do excercise

--- node/8-fs/downloader.js
+++ node/8-fs/downloader.js
@@ -13,21 +13,64 @@ if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir)
 }
 
-files.forEach(file => {
-  const fileUrl = decodeURI(file)
+;(async function () {
+  for (const file of files) {
+    const downloadedFile = await downloadFile(file, dir)
+    console.log(`${downloadedFile.name} downloaded.`)
+  }
+})()
 
-  const fileName = path.basename(fileUrl).replace(/\s/g, '')
+function downloadFile (file, dir) {
+  return new Promise((resolve, reject) => {
+    const fileUrl = decodeURI(file)
 
-  const filePath = path.resolve(dir, fileName)
+    const fileName = path.basename(fileUrl).replace(/\s/g, '')
 
-  const fileStream = fs.createWriteStream(filePath)
+    const filePath = path.resolve(dir, fileName)
 
-  http.get(file, response => {
-    response.pipe(fileStream)
+    const fileStream = fs.createWriteStream(filePath)
 
-    response.on('end', () => {
+    http.get(file, response => {
+      response.pipe(fileStream)
+
+      response.on('end', () => {
         fileStream.close()
-        console.log(`${fileName} Completed`)
+        resolve({ name: fileName, path: filePath, url: fileUrl })
+      })
     })
   })
-})
+}
+
+// const path = require('path')
+// const fs = require('fs')
+// const http = require('https')
+
+// const filePath = path.resolve(__dirname, 'links.txt')
+// const data = fs.readFileSync(filePath, 'utf-8')
+
+// const files = data.split('\n')
+
+// const dir = path.resolve(__dirname, 'downloads')
+
+// if (!fs.existsSync(dir)) {
+//   fs.mkdirSync(dir)
+// }
+
+// files.forEach(file => {
+//   const fileUrl = decodeURI(file)
+
+//   const fileName = path.basename(fileUrl).replace(/\s/g, '')
+
+//   const filePath = path.resolve(dir, fileName)
+
+//   const fileStream = fs.createWriteStream(filePath)
+
+//   http.get(file, response => {
+//     response.pipe(fileStream)
+
+//     response.on('end', () => {
+//         fileStream.close()
+//         console.log(`${fileName} Completed`)
+//     })
+//   })
+// })
