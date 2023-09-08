04383be9c997f564e2dae492c4352c5bd75a1045
setali <set.ali.mousavi@gmail.com>
2023-09-08 12:51:19 +0330

    8-ejs

--- blog/src/controllers/general.js
+++ blog/src/controllers/general.js
@@ -6,14 +6,14 @@ export function home (req, res) {
 }
 
 export function about (req, res) {
-  return res.render('index', {
+  return res.render('about', {
     title: 'About Us Page',
     content: 'About US Page Content'
   })
 }
 
 export function contact (req, res) {
-  return res.render('index', {
+  return res.render('contact', {
     title: 'Contact Us Page',
     content: 'Contact US Page Content'
   })
