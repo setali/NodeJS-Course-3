19c431ec9bdcd9b8d9b1aeccb27dbfaae50da8d9
setali <set.ali.mousavi@gmail.com>
2023-09-08 11:22:53 +0330

    6-custom-engine

--- blog/src/app.js
+++ blog/src/app.js
@@ -1,12 +1,29 @@
 import express from 'express'
 import routes from './routes'
 import errorHandler from './middlewares/error-handler'
-import renderTemplate from './middlewares/render-template'
+import path from 'path'
+import fs from 'fs'
 
 const app = express()
 
+app.engine('ali', (filePath, params, callback) => {
+  let view = fs.readFileSync(filePath, 'utf-8')
+
+  const entries = Object.entries(params)
+
+  entries.forEach(([key, value]) => {
+    if (typeof value === 'string') {
+      view = view.replace(`#${key}#`, value)
+    }
+  })
+
+  return callback(null, view)
+})
+
+app.set('views', path.resolve(__dirname, 'views'))
+app.set('view engine', 'ali')
+
 app.use(express.static('public'))
-app.use(renderTemplate)
 
 app.use(routes)
 
