38a2ecec865b4718864c1e600daf5fc0292a7e94
setali <set.ali.mousavi@gmail.com>
2023-09-22 12:22:33 +0330

    method-override

--- blog/src/middlewares/method-override.js
+++ blog/src/middlewares/method-override.js
@@ -1,8 +1,9 @@
-export default (req, res, next) => {
-  if (req.method === 'POST' && req.body._method) {
-    req.method = req.body._method
-    delete req.body._method
-  }
+import methodOverride from 'method-override'
 
-  next()
-}
+export default methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method
+    delete req.body._method
+    return method
+  }
+})
