919cbce1713156eaab05adb750ace55bb61dc898
setali <set.ali.mousavi@gmail.com>
2023-09-22 11:57:17 +0330

    cru

--- blog/src/controllers/admin/article.js
+++ blog/src/controllers/admin/article.js
@@ -1,8 +1,27 @@
+import { BadRequestError, NotFoundError } from '../../utils/errors'
+
+const articles = []
+
 class ArticleController {
   list (req, res) {
     res.render('admin/article/list', {
       title: 'Article list',
-      articles: []
+      articles
+    })
+  }
+
+  get (req, res) {
+    const { id } = req.params
+
+    const article = articles.find(a => a.id === +id)
+
+    if (!article) {
+      throw new NotFoundError('Article not found')
+    }
+
+    res.render('admin/article/show', {
+      title: article.title,
+      article
     })
   }
 
@@ -13,7 +32,51 @@ class ArticleController {
   }
 
   add (req, res) {
-    res.send(req.body)
+    const { title, text } = req.body
+
+    if (!title || !text) {
+      throw new BadRequestError('Title and Text are required')
+    }
+
+    articles.push({ id: Date.now(), title, text })
+
+    res.status(201).redirect('/admin/article')
+  }
+
+  edit (req, res) {
+    const { id } = req.params
+
+    const article = articles.find(a => a.id === +id)
+
+    if (!article) {
+      throw new NotFoundError('Article not found')
+    }
+
+    res.render('admin/article/edit', {
+      title: `Edit article: ${article.title}`,
+      article
+    })
+  }
+
+  update (req, res) {
+    const { id } = req.params
+
+    const { title, text } = req.body
+
+    if (!title || !text) {
+      throw new BadRequestError('Title and Text are required')
+    }
+
+    const article = articles.find(a => a.id === +id)
+
+    if (!article) {
+      throw new NotFoundError('Article not found')
+    }
+
+    article.title = title
+    article.text = text
+
+    res.redirect(`/admin/article/${id}`)
   }
 }
 
--- blog/src/routes/admin/article.js
+++ blog/src/routes/admin/article.js
@@ -4,8 +4,12 @@ import ArticleController from '../../controllers/admin/article'
 const router = express.Router()
 
 router.get('', ArticleController.list)
+// router.get('/:id([0-9]+)', ArticleController.get)
+router.get('/:id(\\d+)', ArticleController.get)
 router.get('/create', ArticleController.create)
 router.post('', ArticleController.add)
+router.get('/:id(\\d+)/edit', ArticleController.edit)
+router.put('/:id(\\d+)', ArticleController.update)
 
 export default router
 
